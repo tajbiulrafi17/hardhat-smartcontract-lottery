dab107be40523b269c2c9088c5f44bf04943f965
Tajbiul Hassan Rafi <tajbiul.rafi@northsouth.edu>
2023-01-14 01:47:34 +0600

    Contract deployed

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. it cost 0.25 link per request
+const GAS_PRICE_LINK = 1e9 //link per gas. calculated value based on gas price of tthe chain
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if (developmentChains.includes(network.name)) {
+        log("Local network detected! Deploying...")
+
+        //deploy mock vrfcoordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks deployed")
+        log("-------------------------------------------")
+    }
+}
+
+module.exports.tags = ["all", "mocks"]
